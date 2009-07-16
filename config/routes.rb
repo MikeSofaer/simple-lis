0d815dbba2f67e4228a8fe81c355df6795f2950a
Andrew Ng <ayn@andrewng.com>
2009-07-15 22:18:40 -0700

    shallow routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ":resource/:sourced_id", :controller => 'lis', :action => 'destroy', :conditions => { :method => :delete }
   
   map.connect ":parent/:parent_sourced_id/:resource", :controller => 'lis', :action => 'index', :conditions => { :method => :get }
-  map.connect ":parent/:parent_sourced_id/:resource", :controller => 'lis', :action => 'update', :conditions => { :method => :put }
   map.connect ":parent/:parent_sourced_id/:resource/:sourced_id", :controller => 'lis', :action => 'show', :conditions => { :method => :get }
-  map.connect ":parent/:parent_sourced_id/:resource/:sourced_id", :controller => 'lis', :action => 'destroy', :conditions => { :method => :delete }
     
   # controllers = ['people', 'course_templates', 'course_offerings', 'course_sections', 'groups', 'memberships', 'terms', 'meetings']
   # 
--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -27,13 +27,5 @@ describe LisController do
     it "generates params for #show" do
       params_from(:get, '/terms/1/people/1').should == { :action => "show", :resource => "people", :controller => "lis", :sourced_id => "1", :parent => 'terms', :parent_sourced_id => '1' }
     end
-    
-    it "generates params for #update" do
-      params_from(:put, '/terms/1/people').should == { :action => "update", :resource => "people", :controller => "lis", :parent => 'terms', :parent_sourced_id => '1' }
-    end
-    
-    it "generates params for #destroy" do
-      params_from(:delete, '/terms/1/people/1').should == { :action => "destroy", :resource => "people", :controller => "lis", :sourced_id => "1", :parent => 'terms', :parent_sourced_id => '1' }
-    end
   end
 end
