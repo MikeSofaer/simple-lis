7db834a181d1d017e23756d87d3c173524f71791
msofaer <msofaer@inigral.com>
2009-07-22 21:27:16 -0700

    Transactions, but not in tests

--- app/controllers/lis_controller.rb
+++ app/controllers/lis_controller.rb
@@ -24,27 +24,60 @@ class LisController < ActionController::Base
   end
   
   def update
-    begin
-      objects = model.parse_multiple(request.body)
-      if objects.size == 0
-        render :xml => "We weren't able to parse any data from that.  Are you sure the XMl is valid?", :status => :unprocessable_entity and return
+    t = Time.now
+    logger.debug "Start"
+    objects = model.parse_multiple(request.body)
+    logger.debug "Built objects at T + " + (Time.now - t).to_s
+    render :xml => "We weren't able to parse any data from that.  Are you sure the XMl is valid?", :status => :unprocessable_entity and return  if objects.size == 0
+
+    ids = []
+    unless RAILS_ENV == 'test'
+      transaction = DataMapper::Transaction.new(DataMapper.repository)
+      transaction.begin
+      DataMapper.repository(:default).adapter.push_transaction(transaction)
+      begin
+        while objects.size > 0
+          s=objects.slice!(0, 1000)
+          sourced_ids = s.map(&:sourced_id)
+          logger.debug { "saved slice, sourcedIds are #{sourced_ids*', '}" }
+          model.save s
+          ids << sourced_ids
+        end
+        logger.debug "Saved objects at T + " + (Time.now - t).to_s
+        logger.debug "Didn't link objects at T + " + (Time.now - t).to_s
+        DataMapper.repository(:default).adapter.pop_transaction
+        transaction.commit
+      rescue => e
+        DataMapper.repository(:default).adapter.pop_transaction
+        transaction.rollback
+        raise e
       end
-      model.save objects
-      
-      render :xml => objects.map(&:sourced_id).inject('') { |res, sid| res << %Q{<url>#{url_for(:action => 'show', :sourced_id => sid)}</url>} }
-    rescue SAXualReplication::MissingElementError => e
-      render :xml => e.message, :status => :unprocessable_entity and return
-    rescue NoMethodError => e
-      render :xml => "Check your XML, it may be missing tags. \n#{e.message}", :status => :unprocessable_entity and return
-    rescue ActiveRecord::RecordInvalid => e
-      render :xml => e.message, :status => :unprocessable_entity and return
-    rescue Vpim::InvalidEncodingError => e
-      render :xml => "Looks like your ical object is bad at the following line:\n#{e}.", :status => :unprocessable_entity and return
-    rescue TypeError
-      render :xml => "There is something terribly wrong with your request.", :status => :unprocessable_entity and return
-    rescue @@mysql_error
-      render :xml => "Database rejected your request, please make sure all foreign keys are valid", :status => :unprocessable_entity and return
+    else
+      while objects.size > 0
+        s=objects.slice!(0, 1000)
+        sourced_ids = s.map(&:sourced_id)
+        logger.debug { "saved slice, sourcedIds are #{sourced_ids*', '}" }
+        model.save s
+        ids << sourced_ids
+      end
+      logger.debug "Saved objects at T + " + (Time.now - t).to_s
+      logger.debug "Didn't link objects at T + " + (Time.now - t).to_s
     end
+
+    render :xml => ids.flatten.inject('') { |res, sid| res << %Q{<url>#{url_for(:action => 'show', :sourced_id => sid)}</url>} }
+  rescue SAXualReplication::MissingElementError => e
+    render :xml => e.message, :status => :unprocessable_entity and return
+  rescue NoMethodError => e
+    render :xml => "Check your XML, it may be missing tags. \n#{e.message}", :status => :unprocessable_entity and return
+  rescue ActiveRecord::RecordInvalid => e
+    render :xml => e.message, :status => :unprocessable_entity and return
+  rescue Vpim::InvalidEncodingError => e
+    render :xml => "Looks like your ical object is bad at the following line:\n#{e}.", :status => :unprocessable_entity and return
+  rescue TypeError
+    render :xml => "There is something terribly wrong with your request.", :status => :unprocessable_entity and return
+  rescue @@mysql_error => e
+    logger.info { "MYSQL ERROR! error is #{e.message}" }
+    render :xml => "Database rejected your request, please make sure all foreign keys are valid", :status => :unprocessable_entity and return
   end
   
   def destroy
