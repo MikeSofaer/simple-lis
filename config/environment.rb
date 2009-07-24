94d6e5e1368eaaea266003c3deb18dbb43d6da20
msofaer <msofaer@inigral.com>
2009-07-24 13:32:58 -0700

    Don't use my branch of saxual-replicaton

--- config/environment.rb
+++ config/environment.rb
@@ -31,7 +31,6 @@ Rails::Initializer.run do |config|
   config.gem 'do_mysql'
   config.gem "rails_datamapper"
   config.gem "MikeSofaer-saxual-replication", :lib => 'saxual-replication', :source => 'http://gems.github.com'
-  config.gem "MikeSofaer-sax-machine", :lib => 'sax-machine', :source => 'http://gems.github.com'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
