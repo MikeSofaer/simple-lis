0bc63810873d64721da732d7cd75523503606da9
Andrew Ng <ayn@andrewng.com>
2009-08-04 18:46:04 -0700

    membership specs to validate foreign key associations

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -14,4 +14,29 @@ describe "Membership" do
       Membership.parse(@xml.to_s).save!
     end
   end
+  
+  context "foreign_key_valid? tests" do
+    before(:each) do
+      @user = Factory(:person)
+      @org = Factory(:group)
+    end
+    
+    it "should be true with valid user and target" do
+      m = Factory.build(:membership, :target => @org, :person => @user)
+      m.foreign_key_valid?.should be_true
+      lambda{ m.save! }.should_not raise_error
+    end
+    
+    it "should be true without valid user but should get mysqlerror about foreign key constraints" do
+      m = Factory.build(:membership, :target => @org, :person => Factory.build(:person))
+      m.foreign_key_valid?.should be_true
+      lambda{ m.save! }.should raise_error
+    end
+    
+    it "should be false without valid target org" do
+      m = Factory.build(:membership, :target => Factory.build(:group), :person => @user)
+      m.foreign_key_valid?.should be_false
+      lambda{ m.save! }.should raise_error(LISModel::InvalidForeignKeyError)
+    end
+  end
 end
