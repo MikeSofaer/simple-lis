d6effbf8558da5f215462cf036bbfdeff7dfffcf
Andrew Ng <ayn@andrewng.com>
2009-08-04 18:51:09 -0700

    specs to verify that we'll get a 422 when put to :update to meeting with invalid target_sourced_id

--- spec/controllers/lis_controller_spec.rb
+++ spec/controllers/lis_controller_spec.rb
@@ -79,10 +79,43 @@ describe LisController do
 
   context "Meetings" do
     describe "put requests" do
-      it "should succeed at a put with an OK meeting" do
+      it "should fail at a put with a meeting with unknown section" do
         request.env['RAW_POST_DATA'] = "<meetings><meeting>
     <sourced_id>sample2</sourced_id>
-    <target_sourced_id>sample</target_sourced_id>
+    <target_sourced_id>does-not-exist</target_sourced_id>
+    <target_type>Section</target_type>
+    <i_calendar>BEGIN:VCALENDAR
+CALSCALE:GREGORIAN
+PRODID:-//Apple Computer, Inc//iCal 1.5//EN
+VERSION:2.0
+METHOD:REPLY
+BEGIN:VEVENT
+CLASS:PUBLIC
+DTEND;TZID=Pacific:20040415T130000
+DTSTAMP:20040319T205045Z
+DTSTART;TZID=Pacific:20040415T120000
+SEQUENCE:0
+SUMMARY:hjold intyel
+TRANSP:OPAQUE
+UID:3E19204063C93D2388256E5C006BF8D9-Lotus_Notes_Generated
+X-LOTUS-BROADCAST:FALSE
+X-LOTUS-CHILDUID:3E19204063C93D2388256E5C006BF8D9
+X-LOTUS-NOTESVERSION:2
+X-LOTUS-NOTICETYPE:I
+X-LOTUS-UPDATE-SEQ:1
+X-LOTUS-UPDATE-WISL:$S:1;$L:1;$B:1;$R:1;$E:1
+END:VEVENT
+END:VCALENDAR
+</i_calendar>
+    </meeting></meetings>"
+        put :update, :resource => 'meetings'
+        response.status.should == "422 Unprocessable Entity"
+      end
+
+      it "should suceed at a put with a good meeting" do
+        request.env['RAW_POST_DATA'] = "<meetings><meeting>
+    <sourced_id>sample2</sourced_id>
+    <target_sourced_id>#{Factory(:course_section).sourced_id}</target_sourced_id>
     <target_type>Section</target_type>
     <i_calendar>BEGIN:VCALENDAR
 CALSCALE:GREGORIAN
@@ -112,7 +145,6 @@ END:VCALENDAR
         response.status.should == "200 OK"
       end
     end
-    
   end
 
   context "Memberships" do
