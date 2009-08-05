24d8551e4769a6043a41e051b8ed2d8146f0ec50
Andrew Ng <ayn@andrewng.com>
2009-08-04 18:47:23 -0700

    meeting specs to validate foreign key associations

--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -14,4 +14,22 @@ describe "Meeting" do
       Meeting.parse(@xml.to_s).save!
     end
   end
+  
+  context "foreign_key_valid? tests" do
+    before(:each) do
+      @org = Factory(:group)
+    end
+    
+    it "should be true with valid user and target" do
+      m = Factory.build(:meeting, :target => @org)
+      m.foreign_key_valid?.should be_true
+      lambda{ m.save! }.should_not raise_error
+    end
+    
+    it "should be false without valid target org" do
+      m = Factory.build(:meeting, :target => Factory.build(:group))
+      m.foreign_key_valid?.should be_false
+      lambda{ m.save! }.should raise_error(LISModel::InvalidForeignKeyError)
+    end
+  end
 end
